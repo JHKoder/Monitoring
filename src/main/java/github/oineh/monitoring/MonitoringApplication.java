ea14aa85c854663d66789a82d560e56853af1d8b
kang's <jeonghun.kang.dev@gmail.com>
2022-12-28 00:07:23 +0900

    Initial commit

---
+++ src/main/java/github/oineh/monitoring/MonitoringApplication.java
@@ -0,0 +1,13 @@
+package github.oineh.monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonitoringApplication.class, args);
+    }
+
+}
