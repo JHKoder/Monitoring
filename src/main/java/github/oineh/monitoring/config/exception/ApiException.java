974f7fbd93ca2a2ae8aa803463a76e7c00393525
kang's <jeonghun.kang.dev@gmail.com>
2023-01-05 08:59:27 +0900

    :sparkles: add ApiException extends RuntimeException

---
+++ src/main/java/github/oineh/monitoring/config/exception/ApiException.java
@@ -0,0 +1,14 @@
+package github.oineh.monitoring.config.exception;
+
+import lombok.Getter;
+
+@Getter
+public class ApiException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+
+    public ApiException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+}
