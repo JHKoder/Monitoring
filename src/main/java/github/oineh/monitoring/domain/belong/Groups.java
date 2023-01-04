6f84684ee917b49e32f47076a10d6295f2049b77
kang's <jeonghun.kang.dev@gmail.com>
2023-01-05 08:41:31 +0900

    :art: import change groups entity
    spring boot 2.5 -> 2.7 import change

--- src/main/java/github/oineh/monitoring/domain/belong/Groups.java
+++ src/main/java/github/oineh/monitoring/domain/belong/Groups.java
@@ -3,12 +3,12 @@ package github.oineh.monitoring.domain.belong;
 
 import static lombok.AccessLevel.PROTECTED;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
