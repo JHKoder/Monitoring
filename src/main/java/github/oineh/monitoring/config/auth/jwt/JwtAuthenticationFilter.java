7a047d494a18814e03c64b0ca43b642b88ab078e
kang's <jeonghun.kang.dev@gmail.com>
2023-01-05 08:55:59 +0900

    :sparkles: add JWT Authentication Filter

---
+++ src/main/java/github/oineh/monitoring/config/auth/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,51 @@
+package github.oineh.monitoring.config.auth.jwt;
+
+import github.oineh.monitoring.config.auth.UserLogin;
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+    private final JWTUtil jwtUtil;
+
+    @SneakyThrows
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+        throws AuthenticationException {
+
+        if (!request.getMethod().equals("POST")) {
+            throw new AuthenticationServiceException("Authentication method not supported: " + request.getMethod());
+        }
+        String username = obtainUsername(request);
+        username = (username != null) ? username.trim() : "";
+        String password = obtainPassword(request);
+        password = (password != null) ? password : "";
+
+        UsernamePasswordAuthenticationToken token = UsernamePasswordAuthenticationToken.unauthenticated(username,
+            password);
+
+        return authenticationManager.authenticate(token);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+        Authentication authResult) throws IOException {
+        UserLogin userToken = UserLogin.ofPricipal(authResult.getPrincipal());
+        response.addCookie(new Cookie(JWTUtil.BEARER,
+            JWTUtil.BEARER + jwtUtil.generate(userToken.getUsername(), TokenType.ACCESS)));
+    }
+
+}
