7ec19990e6af09f8d49d8ce39644b4ec9bb1621e
kang's <jeonghun.kang.dev@gmail.com>
2023-01-05 08:26:38 +0900

    :sparkles: Add User Repository

---
+++ src/main/java/github/oineh/monitoring/domain/user/UserRepository.java
@@ -0,0 +1,11 @@
+package github.oineh.monitoring.domain.user;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByLoginId(String username);
+
+}
