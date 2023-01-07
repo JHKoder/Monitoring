3874734b6ea00567d299505b7f3aaa25c0339d76
kang's <jeonghun.kang.dev@gmail.com>
2023-01-07 20:44:58 +0900

    :sparkles: pc util add

---
+++ src/main/java/github/oineh/monitoring/domain/pc/PcClientIpUtils.java
@@ -0,0 +1,42 @@
+package github.oineh.monitoring.domain.pc;
+
+import github.oineh.monitoring.config.exception.ApiException;
+import github.oineh.monitoring.config.exception.ErrorCode;
+import io.github.sno.network.Host;
+import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class PcClientIpUtils {
+
+    private static final String[] IP_HEADER_CANDIDATES = {
+        "X-Forwarded-For",
+        "Proxy-Client-IP",
+        "WL-Proxy-Client-IP",
+        "HTTP_X_FORWARDED_FOR",
+        "HTTP_X_FORWARDED",
+        "HTTP_X_CLUSTER_CLIENT_IP",
+        "HTTP_CLIENT_IP",
+        "HTTP_FORWARDED_FOR",
+        "HTTP_FORWARDED",
+        "HTTP_VIA",
+        "REMOTE_ADDR"
+    };
+
+    public static Host filterIp(HttpServletRequest request) {
+        try {
+            for (String header : IP_HEADER_CANDIDATES) {
+                String ipList = request.getHeader(header);
+                if (ipList != null && ipList.length() != 0 && !"unknown".equalsIgnoreCase(ipList)) {
+                    return Host.from(ipList.split(",")[0]);
+                }
+            }
+            return Host.from(request.getRemoteAddr());
+        } catch (Exception e) {
+            throw new ApiException(ErrorCode.CLIENT_IP6V_NO);
+        }
+    }
+}
+
