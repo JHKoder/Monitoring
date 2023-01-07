713551e573fd03e4d0f5962970314c6606ae2e03
Kang JeoungHun <jeonghun.kang.dev@gmail.com>
2023-01-07 20:49:22 +0900

    Feat/pc (#20)
    * :recycle: Pc entity update
    
    * :sparkles: pc util add
    
    * :sparkles: add pc controller
    
    * :sparkles: add pc repository
    
    * :sparkles: add pc service
    
    * :white_check_mark: add pc entity test
    
    * :sparkles: add pc update req
    
    * :sparkles: add pc byPcReq
    
    * :sparkles: add pc apiController

---
+++ src/main/java/github/oineh/monitoring/domain/pc/PcService.java
@@ -0,0 +1,29 @@
+package github.oineh.monitoring.domain.pc;
+
+import github.oineh.monitoring.config.exception.ApiException;
+import github.oineh.monitoring.config.exception.ErrorCode;
+import github.oineh.monitoring.controller.pc.req.PcAddReq;
+import github.oineh.monitoring.domain.connect.Connect;
+import github.oineh.monitoring.domain.user.User;
+import github.oineh.monitoring.domain.user.UserRepository;
+import io.github.sno.network.Host;
+import javax.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PcService {
+
+    private final UserRepository userRepository;
+
+    // - 내 PC 등록
+    @Transactional
+    public void addUserPc(PcAddReq req, Host host, String loginId) {
+        User user = userRepository.findByLoginId(loginId)
+            .orElseThrow(() -> new ApiException(ErrorCode.NOT_FOUND_USER));
+        user.updatePc(new Pc(req.getPcName(), req.getType()));
+        user.getPc().updateConnect(Connect.icmp(host.toString()));
+    }
+
+}
