cf980df8991c3e5149e84b3d2628034a850a93fe
kang's <jeonghun.kang.dev@gmail.com>
2023-01-07 20:51:06 +0900

    :sparkles: add connect repository

---
+++ src/main/java/github/oineh/monitoring/domain/connect/ConnectRepository.java
@@ -0,0 +1,7 @@
+package github.oineh.monitoring.domain.connect;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConnectRepository extends JpaRepository<Connect, Long> {
+
+}
