8562f9a4a389082b1742449286c01c6f7702edf1
kang's <jeonghun.kang.dev@gmail.com>
2023-01-07 20:45:12 +0900

    :sparkles: add pc controller

---
+++ src/main/java/github/oineh/monitoring/controller/pc/PcController.java
@@ -0,0 +1,11 @@
+package github.oineh.monitoring.controller.pc;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class PcController {
+
+
+}
