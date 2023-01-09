7ac925e6866db554d4e78582f390e95e337b53a6
kang's <jeonghun.kang.dev@gmail.com>
2023-01-10 03:15:09 +0900

    :sparkles: controller update

--- src/main/java/github/oineh/monitoring/controller/group/ApiGroupController.java
+++ src/main/java/github/oineh/monitoring/controller/group/ApiGroupController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -37,7 +38,7 @@ public class ApiGroupController {
     }
 
     @PostMapping("/add/team")
-    public ResponseEntity<Void> createGroupTeam(GroupCreateTeamReq req, Principal principal) {
+    public ResponseEntity<Void> createGroupTeam(@RequestBody GroupCreateTeamReq req, Principal principal) {
         groupService.createGroup(req, principal.getName());
 
         return ResponseEntity.ok().build();
