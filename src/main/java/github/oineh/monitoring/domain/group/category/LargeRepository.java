231868688d968190cf4648e4b32e20b7a5a5d81b
kang's <jeonghun.kang.dev@gmail.com>
2023-01-09 21:11:47 +0900

    :fire: remove Large

--- src/main/java/github/oineh/monitoring/domain/group/category/LargeRepository.java
+++
@@ -1,7 +0,0 @@
-package github.oineh.monitoring.domain.group.category;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LargeRepository extends JpaRepository<Large, Long> {
-
-}
