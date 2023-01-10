2b8723f0736b7ec8f8831acf1b640d13974dfd2a
kang's <jeonghun.kang.dev@gmail.com>
2023-01-11 01:45:21 +0900

    :sparkles: update Team entity

--- src/main/java/github/oineh/monitoring/domain/group/category/Team.java
+++ src/main/java/github/oineh/monitoring/domain/group/category/Team.java
@@ -2,9 +2,11 @@ package github.oineh.monitoring.domain.group.category;
 
 import static lombok.AccessLevel.PROTECTED;
 
+import github.oineh.monitoring.domain.connect.Connect;
 import github.oineh.monitoring.domain.user.User;
 import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -29,6 +31,9 @@ public class Team {
 
     @OneToMany(fetch = FetchType.LAZY)
     private List<User> member = new ArrayList<>();
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    private List<Connect> connects = new ArrayList<>();
     private String name;
 
     public Team(User createUser, String name) {
@@ -40,4 +45,7 @@ public class Team {
         this.member.add(user);
     }
 
+    public void updateConnect(Connect connect) {
+        this.connects.add(connect);
+    }
 }
