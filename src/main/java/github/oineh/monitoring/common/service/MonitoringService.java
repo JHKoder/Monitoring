5707c823f01fe1a2b127ac0b5deebad820b68286
kang's <jeonghun.kang.dev@gmail.com>
2023-01-11 01:42:53 +0900

    :sparkles: add MonitoringService bean

---
+++ src/main/java/github/oineh/monitoring/common/service/MonitoringService.java
@@ -0,0 +1,9 @@
+package github.oineh.monitoring.common.service;
+
+import io.github.sno.network.Monitoring;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MonitoringService extends Monitoring {
+
+}
