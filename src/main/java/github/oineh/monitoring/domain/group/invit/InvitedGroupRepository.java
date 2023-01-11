e264677733d115f74b3c01b3abfa267d8cb6edc9
kang's <jeonghun.kang.dev@gmail.com>
2023-01-12 04:39:11 +0900

    :sparkles: add invite groups team

--- src/main/java/github/oineh/monitoring/domain/group/invit/InvitedGroupRepository.java
+++ src/main/java/github/oineh/monitoring/domain/group/invit/InvitedGroupRepository.java
@@ -1,7 +1,15 @@
 package github.oineh.monitoring.domain.group.invit;
 
+import github.oineh.monitoring.domain.group.category.Team;
+import github.oineh.monitoring.domain.user.User;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InvitedGroupRepository extends JpaRepository<InvitedGroup, Long> {
+
+    Optional<List<InvitedGroup>> findByTargetUser(User targetUser);
+
+    Optional<InvitedGroup> findByTargetUserAndTeam(User targetUser, Team team);
 
 }
