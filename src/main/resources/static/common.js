2996f09a5aba019145440ca5617abd9425612c84
kang's <jeonghun.kang.dev@gmail.com>
2023-01-17 12:57:53 +0900

    :hammer: update html js api

--- src/main/resources/static/common.js
+++ src/main/resources/static/common.js
@@ -27,3 +27,87 @@ async function post(path, body, headers = {}) {
     throw Error(data);
   }
 }
+
+async function get(path, body, headers = {}) {
+
+  const url = `${defaultUrl}/${path}`;
+  const options = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      ...headers,
+    },
+    body: JSON.stringify(body),
+  };
+
+  const res = await fetch(url, options);
+  let data;
+
+  try {
+    data = await res.json();
+  } catch (e) {
+    data = res;
+  }
+
+  if (res.ok) {
+    return data;
+  } else {
+    throw Error(data);
+  }
+}
+
+async function Delete(path, body, headers = {}) {
+
+  const url = `${defaultUrl}/${path}`;
+  const options = {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      ...headers,
+    },
+    body: JSON.stringify(body),
+  };
+
+  const res = await fetch(url, options);
+  let data;
+
+  try {
+    data = await res.json();
+  } catch (e) {
+    data = res;
+  }
+
+  if (res.ok) {
+    return data;
+  } else {
+    throw Error(data);
+  }
+}
+
+async function put(path, body, headers = {}) {
+
+  const url = `${defaultUrl}/${path}`;
+  const options = {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      ...headers,
+    },
+    body: JSON.stringify(body),
+  };
+
+  const res = await fetch(url, options);
+  let data;
+
+  try {
+    data = await res.json();
+  } catch (e) {
+    data = res;
+  }
+
+  if (res.ok) {
+    return data;
+  } else {
+    throw Error(data);
+  }
+}
