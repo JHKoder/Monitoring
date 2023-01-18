6ac225d9cbf93bd06ffe891e20c9a4884b0c779d
kang's <jeonghun.kang.dev@gmail.com>
2023-01-18 18:11:13 +0900

    :hammer: refactor method url update

--- src/main/java/github/oineh/monitoring/controller/team/ApiTeamController.java
+++ src/main/java/github/oineh/monitoring/controller/team/ApiTeamController.java
@@ -14,7 +14,6 @@ import github.oineh.monitoring.domain.groups.group.GroupService;
 import java.security.Principal;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,71 +22,52 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/team")
+@RequestMapping("/api/teams")
 @RequiredArgsConstructor
 public class ApiTeamController {
 
     private final ConnectService connectService;
     private final GroupService groupService;
 
-
-    @GetMapping("/domain/{teamId}")
-    public ResponseEntity<List<TeamInDomainRes>> domainList(@PathVariable("teamId") Long teamId) {
-        List<TeamInDomainRes> res = connectService.findTeamInDomain(teamId);
-
-        return ResponseEntity.ok(res);
+    @GetMapping("/{teamId}/domain")
+    public List<TeamInDomainRes> domainList(@PathVariable("teamId") Long teamId) {
+        return connectService.findTeamInDomain(teamId);
     }
 
-    @GetMapping("/member/{teamId}")
-    public ResponseEntity<List<TeamInMemberRes>> memberList(@PathVariable("teamId") Long teamId) {
-        List<TeamInMemberRes> res = connectService.findTeamInMember(teamId);
-
-        return ResponseEntity.ok(res);
+    @GetMapping("/{teamId}/member")
+    public List<TeamInMemberRes> memberList(@PathVariable("teamId") Long teamId) {
+        return connectService.findTeamInMember(teamId);
     }
 
-    @GetMapping("/ping/domain/{teamId}/connect/{connectId}")
-    public ResponseEntity<TeamInDomainPingRes> findTeamDomain(@PathVariable("teamId") Long teamId,
+    @GetMapping("/{teamId}/connect/{connectId}")
+    public TeamInDomainPingRes findTeamDomain(@PathVariable("teamId") Long teamId,
         @PathVariable Long connectId) {
-        TeamInDomainPingRes res = connectService.findTeamInConnectDomainList(teamId, connectId);
-
-        return ResponseEntity.ok(res);
+        return connectService.findTeamInConnectDomain(teamId, connectId);
     }
 
-    @GetMapping("/ping/member/{teamId}/connect/{connectId}")
-    public ResponseEntity<TeamInMemberPingRes> findTeam(@PathVariable("teamId") Long teamId,
+    @GetMapping("/{teamId}/member/connect/{connectId}")
+    public TeamInMemberPingRes findTeam(@PathVariable("teamId") Long teamId,
         @PathVariable Long connectId) {
-        TeamInMemberPingRes res = connectService.findTeamInConnectMemberList(teamId, connectId);
-
-        return ResponseEntity.ok(res);
+        return connectService.findTeamInConnectMemberList(teamId, connectId);
     }
 
     @PostMapping("/invite")
-    public ResponseEntity<Void> invite(@RequestBody TeamInviteReq req, Principal principal) {
+    public void invite(@RequestBody TeamInviteReq req, Principal principal) {
         groupService.targetUserInvite(req, principal.getName());
-
-        return ResponseEntity.ok().build();
     }
-
 
     @PostMapping("/url")
-    public ResponseEntity<Void> createUrl(@RequestBody TeamCreateUrlReq req) {
+    public void createUrl(@RequestBody TeamCreateUrlReq req) {
         connectService.createUrl(req);
-
-        return ResponseEntity.ok().build();
     }
 
-    @PostMapping("/ip/port")
-    public ResponseEntity<Void> createPort(@RequestBody TeamCreatePortReq req) {
+    @PostMapping("/ip-port")
+    public void createPort(@RequestBody TeamCreatePortReq req) {
         connectService.createIpPort(req);
-
-        return ResponseEntity.ok().build();
     }
 
     @PostMapping("/ip")
-    public ResponseEntity<Void> createIp(@RequestBody TeamCreateIpReq req) {
+    public void createIp(@RequestBody TeamCreateIpReq req) {
         connectService.createIp(req);
-
-        return ResponseEntity.ok().build();
-
     }
 }
