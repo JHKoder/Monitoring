637e5d219cae3812099965f3ea3a15178ee3b421
oineh <jeonghun.kang.dev@gmail.com>
2023-02-06 17:51:44 +0900

    :green_heart: GCP 서버 이전으로 배포 스크립트 수정

--- server_build.sh
+++ server_build.sh
@@ -1,12 +1,7 @@
-jarRoot="build/libs/"
-jarName="Monitoring-0.1.0-RELEASE.jar"
+jarRoot="./build/libs/Monitoring-0.1.0-RELEASE.jar"
 serverId="jeonghun.kang.dev"
 serverIp="34.168.204.241"
 serverRoot=":server"
 
 sh `pwd`/gradlew build
-ssh -i gcp_rsa_key ${serverId}@${serverIp} <<EOF
-${jarRoot}${jarName} ${serverIp}${serverRoot}
-fuser -k 8080/tcp
-java -jar server/${jarName}
-EOF
+scp -i gcp_rsa_key ${jarRoot} ${serverId}@${serverIp}${serverRoot}
